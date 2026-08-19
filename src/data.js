6e06b98fcc47d11bd1bad1e76fa77844bb64fddf
Fang Yanbin <84324717+fangyibo2430@users.noreply.github.com>
2026-08-19 19:15:35 +0800

    Clarify first-year PhD status

--- src/data.js
+++ src/data.js
@@ -23,7 +23,7 @@ export const profile = {
 };
 
 export const about = [
-  `I am a Ph.D. student in Decisions, Operations and Technology at The Chinese
+  `I am a first-year Ph.D. student in Decisions, Operations and Technology at The Chinese
    University of Hong Kong.`,
   `My research explores how artificial intelligence can support complex
    decision-making. My current interests include collaboration among large
